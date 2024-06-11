c40ef366028dd5432ac02ed837688c29d76c5f54
SumitScripts (Scripter) <163373047+SumitScripts@users.noreply.github.com>
2024-06-11 13:14:31 +0530

    Create Loader.lua

---
+++ Gui.lua/Loader.lua
@@ -0,0 +1,34 @@
+--[[ 
+ __      __          _   _       _     
+ \ \    / /         | | (_)     (_)    
+  \ \  / / __   __ _| |_ _  ___  _ ___ 
+   \ \/ / '_ \ / _` | __| |/ _ \| / __|
+    \  /| | | | (_| | |_| | (_) | \__ \
+     \/ |_| |_|\__,_|\__|_|\___/|_|___/
+                                       
+                                       
+
+ Vnatiois Loader 2.0 - By SumitScripts LLC
+ Created by SumitScripts, Aakash
+
+- 🌺 Update 2.0 - What New?
+- Added Blade Ball Script
+- Added Miner cave Script 
+- Added Loader
+
+Fully Code Got Messy So bc code editor like notepad fucked up I Can't Update it :( so  don't expect to update it
+
+Supports On All Executors -Making For Solara :) -
+]]--
+
+local GameP = game.PlaceId
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Vnatiois Loader", -- Required
+	Text = "Checking Place Id", -- Required
+	Icon = "rbxassetid://1234567890" -- Optional
+})
+wait(0.7)
+
+-- Minercave
+
